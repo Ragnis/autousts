6b9bc4f1acab13531ed397361e4face916eafb28
Ragnis Armus <ragnis.armus@gmail.com>
2015-05-04 10:55:23 +0300

    Implement parsing pointer from string

--- db/pointer.go
+++ db/pointer.go
@@ -1,7 +1,10 @@
 package db
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
+	"strconv"
 )
 
 type Pointer struct {
@@ -11,4 +14,20 @@ type Pointer struct {
 
 func (p Pointer) String() string {
 	return fmt.Sprintf("S%02dE%02d", p.Season, p.Episode)
+}
+
+func PointerFromString(s string) (Pointer, error) {
+	re := regexp.MustCompile(`^S(\d+)E(\d+)$`)
+	match := re.FindStringSubmatch(s)
+	if match == nil {
+		return Pointer{}, errors.New("Invalid pointer")
+	}
+
+	season, _ := strconv.Atoi(match[1])
+	episode, _ := strconv.Atoi(match[2])
+
+	return Pointer{
+		Season:  uint(season),
+		Episode: uint(episode),
+	}, nil
 }
